c970e957e256d19a2452242b0e49d892d370f12d
Hyeonsoo Kim <75616329+hsoo173610@users.noreply.github.com>
2020-12-15 16:56:33 +0900

    JS : arrow up

--- main.js
+++ main.js
@@ -22,13 +22,36 @@ navbarMenu.addEventListener('click', (event) => {
     }
     
     scrollIntoView(link);
-     
+
 });
 
 // Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home__contact');
 homeContactBtn.addEventListener('click', () => {
     scrollIntoView('#contact');
+});
+
+// Make home slowly fade to transparent as the window scrolls down (실행X)
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrolly / homeHeight;
+});
+
+// Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll',()=> {
+    if(window.scrollY > homeHeight/2) {
+        arrowUp.classList.add('visible');
+    }else{
+        arrowUp.classList.remove('visible');
+
+    }
+});
+
+// Handle click on the "arrow up" button
+arrowUp.addEventListener('click',() =>{
+    scrollIntoView('#home');
 });
 
 function scrollIntoView(selector){
